e821510cd7b2392e3e6a5915ae73a950be155f06
eflores <eflores@switchfly.com>
2016-01-09 21:25:41 +0000

    adding solution for day 9

---
+++ challenges/day-9.py
@@ -0,0 +1,9 @@
+def find_gcd(a, b):
+    if a == b or b == 0:
+        return a
+    return find_gcd(b, a % b)
+
+
+numbers_from_input = input().strip().split(' ')
+gcd = find_gcd(int(numbers_from_input[0]), int(numbers_from_input[1]))
+print(gcd)
