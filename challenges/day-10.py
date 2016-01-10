92071c22c4b93eb66264fd9b0b7e78e57364a28b
eflores <eflores@switchfly.com>
2016-01-10 22:31:32 +0000

    add solution for day 10

---
+++ challenges/day-10.py
@@ -0,0 +1,16 @@
+# Convert a given number, n, from decimal (base 10) to binary (base 2).
+test_cases = int(input().strip())
+BASE = 2
+binary_numbers = []
+for test_case in range(0, test_cases):
+    n = int(input().strip())
+    result = 0
+    binary_number = []
+    while n != 0:
+        remainder = n % BASE
+        n //= BASE
+        binary_number.insert(0, remainder)
+    binary_numbers.append(''.join(str(number) for number in binary_number))
+
+for binary_number in binary_numbers:
+    print(binary_number)
