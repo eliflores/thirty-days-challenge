420dc491f9add98b68046ec237c14eca780bf331
eflores <eflores@switchfly.com>
2016-01-22 04:44:04 +0000

    solution for day 21

---
+++ challenges/java-solutions/Day21Solution.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Method;
+
+class Printer {
+    public <E> void printArray(E[] array) {
+        for (E e : array) {
+            System.out.println(e);
+        }
+    }
+}
+
+public class Day21Solution {
+
+    public static void main(String args[]) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+            if (name.equals("printArray")) {
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            System.out.println("Method overloading is not allowed!");
+        }
+        assert count == 1;
+    }
+}
