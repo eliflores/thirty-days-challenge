9a2ec5ae8097b87617015903f1676e794947f215
eflores <eflores@switchfly.com>
2016-01-28 17:05:39 +0000

    add solution for day 27

---
+++ challenges/python-solutions/day-27.py
@@ -0,0 +1,16 @@
+print('5')
+#YES
+print('4 3')
+print('0 -1 2 3')
+#NO
+print('3 2')
+print('0 -1 4')
+#YES
+print('6 3')
+print('0 -10 4 5 6 7')
+#NO
+print('7 4')
+print('-1 -2 -3 -4 0 1 2 ')
+#YES
+print('8 5')
+print('-1 0 1 2 3 4 5 6')
